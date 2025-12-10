dadecf232606fbcee603b9088075c58ee275fddc
guerric-k <guerrickernyuy@gmail.com>
2025-12-10 22:52:54 +0000

    update products and fix bug in css to display product details

--- noveau_fit/urls.py
+++ noveau_fit/urls.py
@@ -1,4 +1,4 @@
-"""noveau_fit URL Configuration
+"""boutique_ado URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -15,15 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls.i18n import i18n_patterns
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
+    path('', include('home.urls')),
     path('i18n/', include('django.conf.urls.i18n')),
-    path('accounts/', include('allauth.urls')),  # Include allauth URLs
-    path('', include('home.urls')),  # Home app URLs
+    path('products/', include('products.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
